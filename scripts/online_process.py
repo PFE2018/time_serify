44de13decfe4be07fa8775cfcff35856cd211cb4
f3nr1r <sotistechno@gmail.com>
2018-03-29 19:43:24 -0400

    Modified to different file for online processing - headache - new record with elo

---
+++ scripts/online_process.py
@@ -0,0 +1,259 @@
+import pickle
+import matplotlib.pyplot as plt
+from scipy import signal, interpolate, io
+import numpy as np
+import pywt
+import math
+import pandas as pd
+import biosppy as bp
+
+
+def floor_log(num, base):
+    if num < 0:
+        raise ValueError("Non-negative number only.")
+
+    if num == 0:
+        return 0
+
+    return base ** int(math.log(num, base))
+
+
+class Process(object):
+    def __init__(self, pickled_file='CHAIR_OTIS_SAMUEL_2018_03_22_16_06.p',
+                 ref_file='REF_CHAIR_OTIS_SAMUEL_2018_03_22_16_06.mat', show=True, online=False):
+        self.largest_base = 0
+        self.t_i = []
+        self.interp_x = []
+        self.interp_y = []
+        self.interp_z = []
+        self.freq = []
+        self.fft_x = []
+        self.fft_y = []
+        self.fft_z = []
+        self.ref = []
+        self.ref_hr = []
+        self.ref_time = []
+        self.hr_kinect = []
+        self.kinect_time = []
+        # Select ref type to open
+        is_ref_mat = True if '.mat' in ref_file else False
+        if online:
+            self.online = True
+        else:
+            self.online = False
+            self.data_import(pickled_file, ref_file, show, is_ref_mat)
+
+    # Data import and show #
+    def data_import(self, pickle_name, refname, show=True, is_ref_mat=True):
+        # Import data
+        self.t_i, self.interp_x, self.interp_y, self.interp_z, self.freq, self.fft_x, self.fft_y, self.fft_z = pickle.load(
+            open('../recordings/' + pickle_name, 'rb'))
+        # Cut data to magnitude of 2
+        self.largest_base = floor_log(len(self.t_i), 2)
+
+        self.t_i = self.t_i[:self.largest_base]
+        self.interp_x = self.interp_x[:self.largest_base]
+        self.interp_y = self.interp_y[:self.largest_base]
+        self.interp_z = self.interp_z[:self.largest_base]
+        self.freq = self.freq[:self.largest_base]
+        self.fft_x = self.fft_x[:self.largest_base]
+        self.fft_y = self.fft_y[:self.largest_base]
+        self.fft_z = self.fft_z[:self.largest_base]
+        if is_ref_mat:
+            mat_ecg = io.loadmat('../recordings/' + refname)
+            self.ref = bp.ecg.ecg(signal=mat_ecg['data'][0], sampling_rate=mat_ecg['samplerate'][0][0], show=False)
+            self.ref_time = self.ref[5]
+            self.ref_hr = self.ref[6]
+            self.ref_time = np.arange(0, step=1.0 / mat_ecg['samplerate'][0][0], stop=len(self.ref_hr) / 1000.0)
+
+        else:
+            self.ref = pd.DataFrame.from_csv('../recordings/' + refname)
+            filter_time = self.ref.index.values <= self.largest_base / 20.0
+            self.ref_hr = self.ref.values[filter_time].flatten()
+            self.ref_time = self.ref.index.values[filter_time]
+        if show:
+            self.show_data()
+
+    # Show data and related FFT
+    def show_data(self):
+        # Plot real and interpolated signal
+        plt.figure()
+        plt.subplot(321)
+        plt.title('X motion magnitude')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion (m)')
+        plt.plot(self.t_i, self.interp_x, '-r')
+        plt.subplot(322)
+        plt.title('X Fourier transform')
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude x')
+        plt.plot(self.freq, self.fft_x)
+
+        plt.subplot(323)
+        plt.title('Y motion magnitude')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion (m)')
+        plt.plot(self.t_i, self.interp_y, '-r')
+        plt.subplot(324)
+        plt.title('Y Fourier transform')
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude y')
+        plt.plot(self.freq, self.fft_y)
+
+        plt.subplot(325)
+        plt.title('Z motion magnitude')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion (m)')
+        plt.plot(self.t_i, self.interp_z, '-r')
+        plt.subplot(326)
+        plt.title('Z Fourier transform')
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude z')
+        plt.plot(self.freq, self.fft_z)
+
+        plt.pause(0.000001)
+
+    # Wavelet processing and show #
+    def wvt_proc(self, show=True):
+        num_level = int(np.log2(self.largest_base))
+        slct_lvl = 4
+        for axis in [self.interp_x, self.interp_y, self.interp_z]:
+            wlt = pywt.Wavelet('db6')
+            new_sig = pywt.swt(axis, wavelet=wlt, level=num_level)
+
+            if show:
+                plt.figure()
+                plt.subplot(421)
+                plt.title('Wavelet coefficient 1')
+                plt.plot(self.t_i, new_sig[6][1])
+                plt.subplot(422)
+                plt.title('Wavelet coefficient 2')
+                plt.plot(self.t_i, new_sig[5][1])
+                plt.subplot(423)
+                plt.title('Wavelet coefficient 3')
+                plt.plot(self.t_i, new_sig[4][1])
+                plt.subplot(424)
+                plt.title('Wavelet coefficient 4')
+                plt.plot(self.t_i, new_sig[3][1])
+                plt.subplot(425)
+                plt.title('Wavelet coefficient 5')
+                plt.plot(self.t_i, new_sig[2][1])
+                plt.subplot(426)
+                plt.title('Wavelet coefficient 6')
+                plt.plot(self.t_i, new_sig[1][1])
+                plt.subplot(427)
+                plt.title('Wavelet coefficient 7')
+                plt.plot(self.t_i, new_sig[0][1])
+
+            # Get peaks-locs, compute interval
+            loc_idx = signal.find_peaks_cwt(new_sig[num_level - slct_lvl][1], np.arange(1, 10))
+            if show:
+                # Plot slected wavelet coefficient peaks
+                plt.subplot(420 + slct_lvl)
+                plt.plot(self.t_i[loc_idx], new_sig[num_level - slct_lvl][1][loc_idx])
+                plt.pause(0.000001)
+            peaks = {'Time': loc_idx * 1.0 / 20.0}
+            peaks_df = pd.DataFrame(peaks)
+
+            # Compute mean interval and get heart rate with sliding window
+            if self.online:
+                hr = (60.0 / (peaks_df.diff().mean().values)).flatten()
+            else:
+                hr = (60.0 / (peaks_df.diff().rolling(10).mean().values)).flatten()
+            hr_time = peaks_df.values.flatten()
+
+            if not self.online:
+                # Fit data and ref together
+                avoid_nan = ~np.isnan(hr)
+                hr = hr[avoid_nan]
+                hr_time = hr_time[avoid_nan]
+                self.ref_hr = self.ref_hr[self.ref_time >= hr_time[0]]
+                self.ref_time = self.ref_time[self.ref_time >= hr_time[0]]
+                hr = hr[hr_time >= self.ref_time[0]]
+                hr_time = hr_time[hr_time >= self.ref_time[0]]
+
+
+                # Get error with ref
+                interp_ref_f = interpolate.interp1d(self.ref_time, self.ref_hr)
+                interp_ref = interp_ref_f(hr_time)
+                # Basic statistical analysis
+                error = abs(interp_ref - hr)
+                error_m = np.mean(error)
+                error_std = np.std(error)
+                m, b = np.polyfit(interp_ref, hr, 1)
+
+            self.hr_kinect.append(hr)
+            self.kinect_time.append(hr_time)
+
+            if show:
+                # Show analysis
+                plt.figure()
+                plt.plot(hr_time, hr)
+                if self.online:
+                    plt.plot(hr_time, interp_ref)
+                plt.legend(['Kinect measurement', 'ECG Ground truth'])
+                plt.pause(0.000001)
+                if self.online:
+                    plt.figure()
+                    plt.plot(hr_time, error)
+                    plt.pause(0.000001)
+                    # plt.figure()
+                    # plt.plot(interp_ref, hr,'*')
+                    # plt.plot(interp_ref, m * interp_ref + b, '-')
+                    # plt.pause(0.000001)
+
+
+    # STFT processing and show #
+    def stft_proc(self, sig, show=True):
+        specgram, t, z = signal.stft(sig, 20, nperseg=200)
+
+        if show:
+            plt.figure()
+            plt.pcolormesh(t, specgram, np.abs(z))
+            plt.title('STFT Magnitude')
+            plt.ylabel('Frequency [Hz]')
+            plt.xlabel('Time [sec]')
+            plt.pause(0.000001)
+
+
+if __name__ == '__main__':
+    data = Process(pickled_file='CHAIR_OTIS_SAMUEL_2018_03_22_16_06.p',
+                   ref_file='REF_CHAIR_OTIS_SAMUEL_2018_03_22_16_06.mat', show=False)
+    data.wvt_proc([data.interp_x], show=False)
+
+    # Get range fitting for kinect values
+    kinect_hr_end = min(data.kinect_time[0][-1], data.kinect_time[1][-1], data.kinect_time[2][-1])
+    data.ref_hr = data.ref_hr[data.ref_time <= kinect_hr_end]
+    data.ref_time = data.ref_time[data.ref_time <= kinect_hr_end]
+
+    # interpolate axis over ref with right range
+    hr_x_f = interpolate.interp1d(data.kinect_time[0], data.hr_kinect[0], fill_value="extrapolate")
+    hr_y_f = interpolate.interp1d(data.kinect_time[1], data.hr_kinect[1], fill_value="extrapolate")
+    hr_z_f = interpolate.interp1d(data.kinect_time[2], data.hr_kinect[2], fill_value="extrapolate")
+
+    hr_x = np.array(hr_x_f(data.ref_time))
+    hr_y = np.array(hr_y_f(data.ref_time))
+    hr_z = np.array(hr_z_f(data.ref_time))
+    hr = np.array([hr_x, hr_y, hr_z])
+    mean_hr = np.mean(hr, axis=0)
+
+
+    # Plot results
+    plt.figure()
+    plt.plot(data.ref_time, data.ref_hr)
+    plt.plot(data.ref_time, hr_x)
+    plt.plot(data.ref_time, hr_y)
+    plt.plot(data.ref_time, hr_z)
+    plt.legend(['Ground  truth', 'HR x axis', 'HR y axis', 'HR z axis'])
+    plt.pause(0.000001)
+    plt.figure()
+    plt.plot(data.ref_time, data.ref_hr)
+    plt.plot(data.ref_time, mean_hr)
+    plt.pause(0.000001)
+    plt.figure()
+    plt.plot(data.ref_time, abs(data.ref_hr-mean_hr))
+    mean = np.mean(abs(data.ref_hr-mean_hr))
+    dev = np.std(abs(data.ref_hr-mean_hr))
+    plt.pause(0.000001)
+
+    assert True
---
+++ scripts/online_record.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import rospy
+from sensor_msgs.msg import Image, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+from cv_bridge import CvBridge, CvBridgeError
+import scipy.fftpack as fft
+from scipy.interpolate import interp1d
+from scipy.signal import butter, lfilter, filtfilt
+from time import time
+import pickle
+from scripts.offline_process import Process, floor_log
+
+
+class SeriesConverter(object):
+    def __init__(self, online = False):
+        self.image = None
+        self.timeserie = dict({"time": np.empty([0, 1]), "values": np.empty([0, 3])})
+        self.Fs = 20.0  # Herz
+        self.wd = 10.0  # seconds
+        self.t_i = []
+        self.online = online
+        self.data = Process(show=False, online=True)
+        self.shift = False
+        self.update = 20
+
+    def get_values2d_cb(self, msg):
+        bridge = CvBridge()
+        t = (msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9)
+        try:
+            self.image = bridge.imgmsg_to_cv2(msg, "16UC1")
+            value = self.image[230][250]
+            self.timeserie["time"].append(t)
+            self.timeserie["values"].append(value)
+            # INSERT PROCESSING HERE ###############################################
+            if len(self.timeserie["values"]) == 300:  # Windows of last 5 seconds
+                self.timeserie["time"] = np.asarray(self.timeserie["time"])
+                self.timeserie["values"] = np.asarray(self.timeserie["values"])
+                self.do_fft()
+                # Reset window's buffers
+                self.timeserie = dict({"time": [], "values": []})
+            ########################################################################
+        except CvBridgeError or TypeError as e:
+            print(e)
+
+    def get_xyz_cb(self, msg):
+        xyz = []
+        # Extract list of xyz coordinates of point cloud
+        for p in pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True):
+            xyz.append(p)
+        value = np.mean(np.asarray(xyz), axis=0)
+        t = (msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9)
+
+        # Remove old  points if shifting is activated
+        if self.shift:
+            self.timeserie["time"] = self.timeserie["time"][1:]
+            self.timeserie["values"] = self.timeserie["values"][1:,:]
+        # Store  points
+        self.timeserie["time"] = np.append(self.timeserie["time"], t)
+        self.timeserie["values"] = np.vstack([self.timeserie["values"], value])
+        delta = t - self.timeserie["time"][0]
+        # print(str(delta) + 'seconds elapsed')
+        if delta >= self.update:
+            #Start shifting
+            self.shift = True
+            self.update = 2
+            # Transfer to numpy array
+            print len(self.timeserie["time"])
+
+            # Interpolate at fixed frequency
+            span = range(self.timeserie["time"])
+            self.t_i = np.arange(self.timeserie["time"][0], self.timeserie["time"][-1], span/self.timeserie["time"].size)
+            interp_x = interp1d(self.timeserie["time"], self.timeserie["values"][:, 0])
+            interp_x = self.butter_bandpass_filter(interp_x(self.t_i), 0.75, 4)
+            freq, fft_x = self.do_fft(interp_x)
+            interp_y = interp1d(self.timeserie["time"], self.timeserie["values"][:, 1])
+            interp_y = self.butter_bandpass_filter(interp_y(self.t_i), 0.75, 4)
+            _, fft_y = self.do_fft(interp_y)
+            interp_z = interp1d(self.timeserie["time"], self.timeserie["values"][:, 2])
+            interp_z = self.butter_bandpass_filter(interp_z(self.t_i), 0.75, 4)
+            _, fft_z = self.do_fft(interp_z)
+
+            if self.online:
+                # Keep only data for binary size until end
+                self.data.largest_base = floor_log(len(self.t_i), 2)
+                binary_range = self.data.t_i.size - self.data.largest_base
+                self.data.t_i = self.t_i[binary_range:]
+                self.data.interp_x = interp_x[binary_range:]
+                self.data.interp_y = interp_y[binary_range:]
+                self.data.interp_z = interp_z[binary_range:]
+                self.data.freq = freq[binary_range:]
+                self.data.fft_x = fft_x[binary_range:]
+                self.data.fft_y = fft_y[binary_range:]
+                self.data.fft_z = fft_z[binary_range:]
+                # self.data.show_data()
+                self.data.wvt_proc(show=False)
+                print(str(np.mean(self.data.hr_kinect)))
+            else:
+                print('Enter filename...')
+                name = input()
+                pickle.dump((self.t_i, interp_x, interp_y, interp_z, freq, fft_x, fft_y, fft_z),
+                            open(name + '.p', 'wb'))
+
+            if not self.online:
+               self.show_xyz()
+
+    def show_xyz(self):
+        # Plot real and interpolated signal
+        plt.figure(1)
+        plt.clf()
+        plt.subplot(211)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion x (m)')
+        plt.plot(self.timeserie["time"], self.timeserie["values"][:, 0])
+        plt.plot(self.t_i, self.data.interp_x, '-r')
+        plt.subplot(212)
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude x')
+        plt.plot(self.data.freq, self.data.fft_x)
+
+        # float bpm=freq*60
+
+
+        plt.figure(2)
+        plt.clf()
+        plt.subplot(211)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion y (m)')
+        plt.plot(self.timeserie["time"], self.timeserie["values"][:, 1])
+        plt.plot(self.t_i, self.data.interp_y, '-r')
+        plt.subplot(212)
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude y')
+        plt.plot(self.data.freq, self.data.fft_y)
+
+        plt.figure(3)
+        plt.clf()
+        plt.subplot(211)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Motion z (m)')
+        plt.plot(self.timeserie["time"], self.timeserie["values"][:, 2])
+        plt.plot(self.t_i, self.data.interp_z, '-r')
+        plt.subplot(212)
+        plt.xlabel('Frequency (hz)')
+        plt.ylabel('Amplitude z')
+        plt.plot(self.data.freq, self.data.fft_z)
+        plt.pause(0.000001)
+
+    def butter_bandpass(self, lowcut, highcut, order=5):
+        nyq = 0.5 * self.Fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype = 'band')
+        return b, a
+
+    def butter_bandpass_filter(self, data, lowcut, highcut):
+        b, a = self.butter_bandpass(lowcut, highcut)
+        y = filtfilt(b, a, data)
+        return y
+
+    def do_fft(self, values):
+        Fs = self.Fs  # sampling rate
+        Ts = 1.0 / Fs  # sampling interval
+
+        n = len(self.t_i)  # length of the signal
+        frq = np.linspace(0.0, Fs / 2.0, n / 2)  # one side frequency range
+
+        Y = fft.fft(values) / n  # fft computing and normalization
+        Y = 2.0 / n * np.abs(Y[:n // 2])
+
+        return frq, Y
+
+
+
+if __name__ == '__main__':
+    counter = 0
+    timeseries = SeriesConverter(online=True)
+    plt.ion()
+    plt.show()
+    rospy.init_node("time_series_prcss")
+    # rospy.Subscriber("/kinect2/sd/image_depth", Image, timeseries.get_values2d_cb)
+    rospy.Subscriber("/filtered_pcloud", PointCloud2, timeseries.get_xyz_cb)
+    rospy.spin()
